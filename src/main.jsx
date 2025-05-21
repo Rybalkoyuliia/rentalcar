411d6d7234c29ff3124479ab3f25ef395956b26b
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2025-05-21 10:07:09 -0500

    deploy fixed basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/rentalcar">
+  <BrowserRouter basename="/rentalcar/">
     <StrictMode>
       <App />
     </StrictMode>
